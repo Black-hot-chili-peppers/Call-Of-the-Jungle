069f4514b27c7f3f78214ac9aed44df7e0593dfc
Reinsky553 <79757046+Reinsky553@users.noreply.github.com>
2024-06-20 11:56:11 +0700

    Add files via upload

--- Player.gd
+++ Player.gd
@@ -7,17 +7,21 @@ var screen_size
 var target_velocity = Vector2.ZERO
 var is_game_ended:bool = false
 @onready var Text = $"../WinText"
+@onready var WalkSound = $"../AudioStreamPlayer"
 
 func _ready():
 	screen_size = get_viewport_rect().size
+
+func _walk():
+	await get_tree().create_timer(0.1).timeout
+	$"../AudioStreamPlayer".playing = true
 
 func _physics_process(delta):
 	target_velocity = Vector2.ZERO
 	 # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		target_velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		target_velocity.x -= 1
+	if Input.is_action_pressed("move_left"):		target_velocity.x -= 1
 	if Input.is_action_pressed("move_down"):
 		target_velocity.y += 1
 	if Input.is_action_pressed("move_up"):
@@ -27,6 +31,10 @@ func _physics_process(delta):
 		if target_velocity.length() > 0:
 			target_velocity = target_velocity.normalized() * speed
 			$AnimatedSprite2D.play()
+			if !WalkSound.playing:
+				WalkSound.play()
+		elif WalkSound.playing:
+			WalkSound.stop()
 		else:
 			$AnimatedSprite2D.stop()
 
@@ -44,7 +52,6 @@ func _physics_process(delta):
 			$AnimatedSprite2D.flip_v = velocity.y > 0
 
 
-	
 	# Replace with function body.
 func start(pos):
 	position = pos
